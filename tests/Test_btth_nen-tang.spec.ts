357fcf5c6a7059f10fee638927f1e7bfcbe9c863
quyenlt-1306 <le.thao.quyen@sun-asterisk.com>
2025-12-19 16:27:46 +0700

    quyenlt-playwright-btth-nangcao-1.1-1.4

--- tests/Test_btth_nen-tang.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-test("user can search by any text", async ({page}) => {
-    await page.goto ("https://www.w3schools.com/");
-    await page.locator("#tnb-google-search-input").fill ("TypeScript");
-    await page.locator("#tnb-google-search-submit-btn").click();
-    await expect (page.locator('.gsc-results-wrapper-visible')).toContainText("TypeScript");
-});
